5bfa5944f23906c2047767651386084e6f87ccce
siddbytheway <siddbrokedakeyboard@gmail.com>
2025-08-31 21:12:53 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const http = require('http')
+
+http.createServer((req , res) => {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end("<h1>Hello there </h1>")
+}).listen(3000)
+
+
