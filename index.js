c65cc9c18b372dcd9c3a448172b3ebbb7e803391
siddbytheway <siddbrokedakeyboard@gmail.com>
2025-09-02 12:48:49 +0530

    todoapp

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const express = require("express")
+
+const app = express();
+
+app.set('view engine' , 'ejs')
+
+
+app.get("/" , (req , res)=> {
+
+    res.render('index')
+})
+
+
+app.post("/add" , (req , res) => {
+    
+    console.log(req.body.task);
+})
+
+
+app.listen(3000)
