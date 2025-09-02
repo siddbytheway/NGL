42ac338b1afa51248b84e433ef852f001bab4fcc
siddbytheway <siddbrokedakeyboard@gmail.com>
2025-09-02 21:37:06 +0530

    project completed
    simple to do app

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended:true}))
+
 app.set('view engine' , 'ejs')
 
+let tasks = []
 
 app.get("/" , (req , res)=> {
 
-    res.render('index')
+    res.render('index' , {tasks:tasks})
+    console.log(tasks)
 })
 
 
-app.post("/add" , (req , res) => {
-    
-    console.log(req.body.task);
+app.post("/delete" ,async (req , res) => {
+    let index = await req.body.index
+    console.log(index)
+    if(index){
+        tasks.splice(index , 1);
+    }
+    res.redirect("/")
+})
+
+app.post("/edit" , async (req , res) => {
+    let index = await req.body.index;
+    console.log(index)
+    let newelement = await req.body.new
+
+    console.log(newelement)
+
+    if(index){
+        tasks[index] = newelement;
+    }
+
+    res.redirect("/")
+})
+
+app.post("/add" , async (req , res) => {
+
+    let task = await req.body.task 
+
+    if(task){
+        tasks.push(task);
+    }
+
+    res.redirect("/")
 })
 
 
